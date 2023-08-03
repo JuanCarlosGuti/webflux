0a2609ab7492bf13f37c6e01b5523bc25533df11
Juan Carlos Gutierrez <juancgutierrezh@gmail.com>
2023-08-02 19:35:54 -0500

    first commit

---
+++ reatorProject/src/main/java/com/reactiva/programacion/reatorproject/ReatorProjectApplication.java
@@ -0,0 +1,13 @@
+package com.reactiva.programacion.reatorproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReatorProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReatorProjectApplication.class, args);
+    }
+
+}
